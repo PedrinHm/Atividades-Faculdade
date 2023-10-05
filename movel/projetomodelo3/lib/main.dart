c067d854fc8db486728ed3db6579586c7f30b7ec
Pedro Mendes <pedrohm441@gmail.com>
2023-10-04 21:02:08 -0300

    tela 3 add

--- movel/projetomodelo3/lib/main.dart
+++ movel/projetomodelo3/lib/main.dart
@@ -6,12 +6,32 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Text("teste"),
+        backgroundColor: const Color.fromARGB(255, 229, 180, 255),
+        body: Align(
+        alignment: Alignment.bottomCenter,
+          child: Container(
+            margin: const EdgeInsets.all(0.0),
+            width:  double.infinity,
+            height: 420.0,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50.0),
+                topRight: Radius.circular(50.0)
+                ),
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+            child: const Text(
+              'Spanish language', 
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 33),
+              
+              ),
+          ),
+        ),
       ),
     );
   }
