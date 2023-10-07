0fe94a5629570e7035d07d3af29c0fa7397d82a5
Pedro Mendes <pedrohm441@gmail.com>
2023-10-06 21:16:05 -0300

    movel

--- movel/projetomodelo3/lib/main.dart
+++ movel/projetomodelo3/lib/main.dart
@@ -12,32 +12,43 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 229, 180, 255),
         body: Align(
-        alignment: Alignment.bottomCenter,
-          child: Container(
-            margin: const EdgeInsets.all(0.0),
-            width:  double.infinity,
-            height: 420.0,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(50.0),
-                topRight: Radius.circular(50.0)
+          alignment: Alignment.bottomCenter,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  height: double.maxFinite,
+                  color: const Color.fromRGBO(68, 138, 255, 1),
                 ),
-              color: Color.fromARGB(255, 255, 255, 255),
-            ),
-            child: Container(
-              margin: const EdgeInsets.only(
-                left: 30.0,
-                top: 50.0,    
-                right: 300.0, 
-                bottom: 15.0, 
-               ),
-              child: const Text(
-              'Spanish language', 
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 40),
-
               ),
-            ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(0.0),
+                  width: double.infinity,
+                  height: 420.0,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50.0),
+                        topRight: Radius.circular(50.0)),
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      left: 30.0,
+                      top: 50.0,
+                      right: 300.0,
+                      bottom: 15.0,
+                    ),
+                    child: const Text(
+                      'Spanish language',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 40),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
